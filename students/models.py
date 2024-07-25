62f64b6b6b945adc771b6845039a74c2827ce086
altayjabbar <jabbarofaltay@gmail.com>
2024-07-25 20:09:21 +0400

    add students

--- students/models.py
+++ students/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    student_number = models.PositiveIntegerField()
+    first_name = models.CharField(max_length=50)  # Add max_length
+    last_name = models.CharField(max_length=50)  # Add max_length
+    email = models.EmailField()
+    field_of_study = models.CharField(max_length=100)
+    gpa = models.FloatField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
--- students/views.py
+++ students/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from .models import Student
+
 
 # Create your views here.
+def index(request):
+
+    return render(request, "students/index.html", {"students": Student.objects.all()})
+
+
+def views_student(request, id):
+    student = Student.objects.get.all(pk=id)
+    return HttpResponseRedirect(reverse("index"))
