7ef9d67885a09531a60ce2c53139f0946a1f9959
altayjabbar <jabbarofaltay@gmail.com>
2024-07-27 02:26:04 +0400

    added dockerfile

--- students/views.py
+++ students/views.py
@@ -4,25 +4,26 @@ from django.urls import reverse
 from .models import Student
 from .forms import StudentForm
 
-def index(request):
-    return render(request,'students/index.html',{
-        'students':Student.objects.all()
-    })
 
-def view_student(request,id):
-    student= Student.objects.get(pk=id)
-    return HttpResponseRedirect(reverse('index'))
+def index(request):
+    return render(request, "students/index.html", {"students": Student.objects.all()})
+
+
+def view_student(request, id):
+    student = Student.objects.get(pk=id)
+    return HttpResponseRedirect(reverse("index"))
+
 
 def add(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = StudentForm(request.POST)
         if form.is_valid():
-            new_student_number = form.cleaned_data['student_number']
-            new_first_name = form.cleaned_data['first_name']
-            new_last_name = form.cleaned_data['last_name']
-            new_email = form.cleaned_data['email']
-            new_field_of_study = form.cleaned_data['field_of_study']
-            new_gpa = form.cleaned_data['gpa']
+            new_student_number = form.cleaned_data["student_number"]
+            new_first_name = form.cleaned_data["first_name"]
+            new_last_name = form.cleaned_data["last_name"]
+            new_email = form.cleaned_data["email"]
+            new_field_of_study = form.cleaned_data["field_of_study"]
+            new_gpa = form.cleaned_data["gpa"]
 
             new_student = Student(
                 student_number=new_student_number,
@@ -30,42 +31,38 @@ def add(request):
                 last_name=new_last_name,
                 email=new_email,
                 field_of_study=new_field_of_study,
-                gpa=new_gpa
+                gpa=new_gpa,
             )
             new_student.save()
-            return render(request, 'students/add.html', {
-                'form': StudentForm(),
-                'success': True
-            })
+            return render(
+                request, "students/add.html", {"form": StudentForm(), "success": True}
+            )
         else:
-            return render(request, 'students/add.html', {'form': form})
-    
+            return render(request, "students/add.html", {"form": form})
+
     else:
         form = StudentForm()
 
-    return render(request, 'students/add.html', {'form': form})
+    return render(request, "students/add.html", {"form": form})
 
-def edit(request,id):
-    if request.method == 'POST':
+
+def edit(request, id):
+    if request.method == "POST":
         student = Student.objects.get(pk=id)
-        form= StudentForm(request.POST,instance=student)
+        form = StudentForm(request.POST, instance=student)
         if form.is_valid():
             form.save()
-            return render(request,'students/edit.html',{
-                'form':form,
-                'success':True
-            })
+            return render(
+                request, "students/edit.html", {"form": form, "success": True}
+            )
     else:
         student = Student.objects.get(pk=id)
         form = StudentForm(instance=student)
-        return render(request,'students/edit.html',{
-            'form':form
-        })
+        return render(request, "students/edit.html", {"form": form})
 
-def delete(request,id):
-    if request.method == 'POST':
+
+def delete(request, id):
+    if request.method == "POST":
         student = Student.objects.get(pk=id)
         student.delete()
-        return HttpResponseRedirect(reverse('index'))
-            
-
+        return HttpResponseRedirect(reverse("index"))
